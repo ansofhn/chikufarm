8c3706c7e138d153cb4b663c70862e8cbe8cd592
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-07-21 17:27:33 +0700

    style: add shadow for each component

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="mx-4 my-4 rounded-md text-textColor ml-72">
             <div className="flex justify-end ">
-                <div className="p-3 bg-white rounded-md w-72">Profile</div>
+                <div className="p-3 bg-white rounded-md shadow-sm shadow-shadowColor w-72">Profile</div>
             </div>
         </div>
     );
